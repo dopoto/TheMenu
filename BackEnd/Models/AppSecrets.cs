6f94fb5158e1652260436af694ba39f820ed7179
Dorin Potorac <dopoto@gmail.com>
2021-09-25 07:56:54 +0300

    Re-arrange app secrets.

--- BackEnd/Models/AppSecrets.cs
+++ BackEnd/Models/AppSecrets.cs
@@ -7,12 +7,15 @@
     /// One-time initial setup on dev machines:
     /// - Generate a JSON file from the class below  
     /// <see cref="https://csharp2json.io/"/>
-    ///
     /// - Fill in your data
+    /// - Update your local secrets (right-click project in VS > Manage User Secrets)
     /// 
-    /// - Right-click project in VS > Manage User Secrets
+    /// To modify this list:
+    /// - Update your local secrets (right-click project in VS > Manage User Secrets)
+    /// - Update corresponding property in the class below.
+    /// - Update appsettings.json (leave empty value there)
+    /// - Update Azure Devops > Pipelines > Library
     /// </summary>
-    /// 
     /// <see cref="https://docs.microsoft.com/en-us/aspnet/core/security/app-secrets?view=aspnetcore-6.0&tabs=windows#map-secrets-to-a-poco"/>
     public class AppSecrets
     {
