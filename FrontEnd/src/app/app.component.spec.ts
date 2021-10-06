5d34a830958f184bf1ccefd9cc0f40cdac574a34
Dorin Potorac <dopoto@gmail.com>
2021-10-06 22:08:13 +0300

    Add NO_ERRORS_SCHEMA

--- FrontEnd/src/app/app.component.spec.ts
+++ FrontEnd/src/app/app.component.spec.ts
@@ -1,10 +1,13 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
+
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [AppComponent],
+            schemas: [NO_ERRORS_SCHEMA]
         }).compileComponents();
     });
 
