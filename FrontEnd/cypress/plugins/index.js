3efb89cfe6ee413cbe1c9c75a47f9246fc6e9e39
Dorin Potorac <dopoto@gmail.com>
2021-10-09 08:00:50 +0300

    Set up Cypress xml and html reports -https://dzone.com/articles/7-easy-steps-to-generate-xml-and-html-reports-in-cypress

--- FrontEnd/cypress/plugins/index.js
+++ FrontEnd/cypress/plugins/index.js
@@ -9,6 +9,12 @@
 // https://on.cypress.io/plugins-guide
 // ***********************************************************
 
+const {
+    beforeRunHook,
+    afterRunHook,
+} = require("cypress-mochawesome-reporter/lib");
+const exec = require("child_process").execSync;
+
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
@@ -16,7 +22,22 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
-module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-}
+module.exports = (on) => {
+    on("before:run", async (details) => {
+        console.log("override before:run");
+        await beforeRunHook(details);
+        //If you are using other than Windows remove below two lines
+        await exec(
+            "IF EXIST cypress\\screenshots rmdir /Q /S cypress\\screenshots"
+        );
+        await exec("IF EXIST cypress\\reports rmdir /Q /S cypress\\reports");
+    });
+    on("after:run", async () => {
+        console.log("override after:run");
+        //if you are using other than Windows remove below line (having await exec)
+        await exec(
+            "npx jrm ./cypress/reports/junitreport.xml ./cypress/reports/junit/*.xml"
+        );
+        await afterRunHook();
+    });
+};
