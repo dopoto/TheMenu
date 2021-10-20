0bea9f90662f2aa9273feb01a3f5eb0780c17817
Dorin Potorac <dopoto@gmail.com>
2021-10-20 07:08:10 +0300

    Fix tests

--- FrontEnd/src/app/features/landing/components/start-demo/start-demo.component.spec.ts
+++ FrontEnd/src/app/features/landing/components/start-demo/start-demo.component.spec.ts
@@ -1,25 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
 import { StartDemoComponent } from './start-demo.component';
 
 describe('StartDemoComponent', () => {
-  let component: StartDemoComponent;
-  let fixture: ComponentFixture<StartDemoComponent>;
+    let component: StartDemoComponent;
+    let fixture: ComponentFixture<StartDemoComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ StartDemoComponent ]
-    })
-    .compileComponents();
-  });
+    let store: MockStore;
+    const initialState = {};
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StartDemoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [StartDemoComponent],
+            providers: [provideMockStore({ initialState })],
+        }).compileComponents();
+        
+        store = TestBed.inject(MockStore);
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(StartDemoComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
